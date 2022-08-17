dbae3c5fcf1b46d5a6baf94c5639e4abcbab2185
Kevin Zheng <kevzheng27@gmail.com>
2022-08-16 21:31:24 -0400

    commit tree 7c270437b82277996bc2c39dc979c3dc681c18ea

--- src/components/PlantList.js
+++ src/components/PlantList.js
@@ -1,9 +1,11 @@
 import React from "react";
 import PlantCard from "./PlantCard";
 
-function PlantList() {
+function PlantList({ plantToDisplay }) {
   return (
-    <ul className="cards">{/* render PlantCards components in here */}</ul>
+    <ul className="cards">
+      {plantToDisplay.map((plant) => <PlantCard key={plant.id} plant={plant} />)}
+    </ul>
   );
 }
 
--- src/components/PlantPage.js
+++ src/components/PlantPage.js
@@ -1,14 +1,36 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
 
 function PlantPage() {
+
+  const [plantList, setPlantList] = useState([])
+  const [search, setSearch] = useState('')
+
+  useEffect(() => {
+    fetch('http://localhost:6001/plants')
+    .then((r) => r.json())
+    .then((data) => setPlantList(data))
+  }, [])
+
+  function updateList(data) {
+    const listCopy = [...plantList, data]
+    setPlantList(listCopy)
+  }
+
+
+  const plantToDisplay = plantList.filter((plant) => 
+     plant.name.toLowerCase().includes(search.toLowerCase())
+  )
+
+
+
   return (
     <main>
-      <NewPlantForm />
-      <Search />
-      <PlantList />
+      <NewPlantForm updateList={updateList} />
+      <Search setSearch={setSearch} />
+      <PlantList plantToDisplay={plantToDisplay} />
     </main>
   );
 }
